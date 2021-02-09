8512e5e026b713da33e650d07cba7a6444f12138
Victor Adeshile <davik4life@gmail.com>
2021-02-09 05:09:11 +0100

    Commiting

--- play.js
+++ play.js
@@ -2,5 +2,5 @@
 
 // eslint-disable-next-line no-unused-vars
 function computerPlay() {
-	return ;// Randomly return Rock, Paper or Scissors...
+	return ;// Randomly return Rock, Paper or Scissors.
 }
